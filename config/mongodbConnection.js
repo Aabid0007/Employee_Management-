3c255bbf5f8172afde71a4027dc9915fc6f9003a
Aabid <zainulaabid0007@gmail.com>
2025-03-01 01:10:21 +0530

    final commit

--- config/mongodbConnection.js
+++ config/mongodbConnection.js
@@ -6,7 +6,7 @@ const connectDb = async () => {
         console.log("Database connected: ", connect.connection.host, connect.connection.name);
     } catch (error) {
         console.log(error);
-        process.exist(1);
+        process.exit(1);
     }
 };
 
